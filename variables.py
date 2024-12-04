e9c8cd38c11ee4d007842e73c916439095b3177a
Sushant <stha2022sushant@gmail.com>
2024-12-04 21:52:42 +0545

    basic concept of variables and type change

---
+++ variables.py
@@ -0,0 +1,8 @@
+name = input("Enter name of Employee: ")
+age = int(input("Enter age: "))
+salary = float(input("Enter Salary: "))
+
+age = str(age)
+salary = str(salary)
+
+print(name + " is our hard working employee, his age is " + age + "and we offer him a salary of Rs "+ salary)
