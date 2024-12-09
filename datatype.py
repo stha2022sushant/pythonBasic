bf5a3146fdbd8b6fda15443009efcb3de76fa36b
Sushant <stha2022sushant@gmail.com>
2024-12-09 23:05:37 +0545

    modifying complex number calculations with conditional statements

--- datatype.py
+++ datatype.py
@@ -23,3 +23,28 @@ complexMultiplication = str(complexMultiplication)
 complexDivision = str(complexDivision)
 
 print("The addition of complex number is: " + complexAddition)
+print("The substration of complex number is: " + complexSubstract)
+print("The multiplication of complex number is: " + complexMultiplication)
+print("The Division of complex number is: " + complexDivision)
+
+# let's try some if/else condition
+# to get only required value
+
+#userInput = input("Enter 'A/a' for addition, 'B/b' for substration, 'C/c' for Multiplication, 'D/d' for division")
+
+userInput = input("""\n Enter 'A/a' for addition,
+'B/b' for subtraction,
+'C/c' for multiplication,
+'D/d' for division : """)
+
+if(userInput == 'a'):
+    print(complexAddition)
+elif(userInput == 'b'):
+    print(complexSubstract)
+
+elif(userInput == 'c'):
+    print(complexMultiplication)
+elif(userInput == 'd'):
+    print(complexDivision)
+else:
+    print("Invalid Input")
