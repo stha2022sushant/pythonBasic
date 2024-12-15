60568f2079dfa1497733a9f706f74aea9660ae1c
Sushant <stha2022sushant@gmail.com>
2024-12-15 21:23:43 +0545

    testing multidimentional array functionality

---
+++ 2D_array.py
@@ -0,0 +1,11 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matrix[1][2])  # Access element 6
+
+mixed_list = [1, "hello", 3.14, [5, 6]]
+print(mixed_list[3][0])  # [1, "hello", 3.14, [5, 6]]
+
+
+x = -5
+if x > 0:
+    print("Positive number")
+# Output: Nothing, as the condition is False
