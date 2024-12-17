623360cbf8dcc67c3eb3eb2c97ce02c72a5fb3bd
Sushant <stha2022sushant@gmail.com>
2024-12-17 22:17:29 +0545

    making copy of some program

--- listAdvance.py
+++ listAdvance.py
@@ -3,3 +3,9 @@ list2 = list1.copy()
 list2[0][0] = 99
 print(list1)  # Output: [[99, 2], [3, 4]]
 
+list1 = [[1, 2], [3, 4]]
+list2 = list1.copy()
+list2[0][0] = 99
+print(list1)  # Output: [[99, 2], [3, 4]]
+
+
